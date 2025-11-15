3f3e4a6e6725c0b6af109226edbd2f66af402bd8
myriamleb9 <lebbakherm@gmail.com>
2025-11-15 01:04:00 +0100

    final version

--- Word-Analytics/script.js
+++ Word-Analytics/script.js
@@ -8,18 +8,48 @@ const StatNumFacebookEl = document.querySelector('.Stat_Number_Facebook')
 
 textareaEl.addEventListener("input", () =>
 
-    {
-        const NumberOfChars = textareaEl.value.length;
+    {   // define the counting
 
+        let NumberOfWords = textareaEl.value
+  .trim()
+  .split(/\s+/)
+  .filter(word => word.length > 0)
+  .length;
+
+        const NumberOfChars = textareaEl.value.length;
         let NumberOfTwitter = 280 - NumberOfChars;
         let NumberOfFacebook = 2200 - NumberOfChars;
 
+        if (textareaEl.value.includes("<script>")) {
+            alert("You do not have permission");
+            textareaEl.value.replace('<script>', '');
+        }
+
+    
+
+        // add a new limit 
+
+         if (NumberOfTwitter < 0) {
+             StatNumTwitterEl.classList.add('Limit-number');
+            } else {
+                StatNumTwitterEl.classList.remove('Limit-number');
+            }
+
+              if (NumberOfFacebook < 0) {
+             StatNumFacebookEl.classList.add('Limit-number');
+            } else {
+                StatNumFacebookEl.classList.remove('Limit-number');
+            }
+
+        // Publish da fukkah
 
         StatNumCharsEl.textContent = NumberOfChars;
+        StatNumWordsEl.textContent = NumberOfWords;
         StatNumTwitterEl.textContent = NumberOfTwitter;
         StatNumFacebookEl.textContent = NumberOfFacebook;
 
     }
+
 
 
 )
